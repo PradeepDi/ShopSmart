110ac9c86872d4ffa78dc14494e8136894550fc2
PradeepDi <Pradeepdissanayaka.pm@gmail.com>
2025-03-15 19:42:27 +0530

    api ignore

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 import Constants from 'expo-constants';
 
 // Import environment variables from Expo's Constants manifest
-const GOOGLE_MAPS_API_KEY = Constants.expoConfig?.extra?.GOOGLE_MAPS_API_KEY || 'AIzaSyDJtiDYWhBTnv8YKOdfUmeSjob1e9wyXz8';
+const GOOGLE_MAPS_API_KEY = Constants.expoConfig?.extra?.GOOGLE_MAPS_API_KEY;
 
 // Export configuration variables
 export const config = {
